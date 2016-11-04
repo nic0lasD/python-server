f7545b924a3d946153839ac580522f428c34bda1
HEDGEHOG\nicolas <nico1.delpech@gmail.com>
2016-11-04 10:32:31 +1000

    no message

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import subprocess
 import zmq
 import requests
-import urllib2
 
 
 
@@ -17,11 +16,9 @@ while True:
 	message = message.split('#')
 	ip = message[0]
 	art = message[1]
-	client = message[2]
+	clientImage = message[2]
 	socket.send(b"Style style transfer will begin")
-	artImage = urllib2.urlopen('http://'+art)
-	clientImage = urllib2.urlopen('http://'+client)
-	subprocess.call(["python", "../style-transfer/style.py" ,"-s", artImage, "-c", clientImage, "-m", "googlenet", "-g", "-1"])
+	subprocess.call(["python", "../style-transfer/style.py" ,"-s", "http://"+art, "-c", "http://"+clientImage, "-m", "googlenet", "-g", "-1"])
 
     #  Do some 'work'
 	#files = {'file': ('picture.jpg',open('/style-transfer/outputs/outputname.jpg', 'rb'))}

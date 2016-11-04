c44f930effcbf666f64bd1cf2b76541ecbaa0a86
HEDGEHOG\nicolas <nico1.delpech@gmail.com>
2016-11-04 13:41:29 +1000

    Message

--- server.py
+++ server.py
@@ -18,12 +18,12 @@ while True:
 	art = message[1]
 	clientImage = message[2]
 	socket.send(b"Style style transfer will begin")
-	subprocess.call(["python", "../style-transfer/style.py" ,"-s", "http://"+art, "-c", "http://"+ip+ "/contentuploads/" +clientImage, "-m", "googlenet", "-g", "-1"])
+	subprocess.call(["python", "../style-transfer/style.py" ,"-s", "http://"+art, "-c", "http://"+ip+ "/contentresult/" +clientImage, "-m", "googlenet", "-g", "-1"])
 
     #  Do some 'work'
 	list = os.listdir("../style-transfer/outputs/")
 	files = {'file': (clientImage+'.jpg',open('../style-transfer/outputs/'+list[0], 'rb'))}
-	requests.post("http://"+ip+"/uploadimage", files=files)
+	requests.post("http://"+ip+"/images/results", files=files)
 	os.remove('../style-transfer/outputs/'+list[0])
     #  Send reply back to client
 
